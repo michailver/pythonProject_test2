0fa7da4add463f8240131b2f55fb39dfc41fcb00
michailver <ver.michail@gmail.com>
2024-10-11 11:16:58 +0300

    5 pradedu ir faila pervadinau

---
+++ 2_Stringai_2024_10_11.py
@@ -0,0 +1,50 @@
+########################################################################################################################
+#1. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus (Jonas Jonaitis).
+# Atspausdinti trumpesnį stringą.
+# actor1='Jonas Jonaitis'
+# actor2='Petras Petraitis'
+# if len(actor1) < len(actor2):
+#     print (f'Trumpesnį stringą yra: {actor1}. ilgis {len(actor1)}')
+# elif len(actor1) == len(actor2):
+#     print(f'stringą vienodo ilgio: {actor1}, {actor2}')
+# else:
+#     print(f'Trumpesnį stringą yra: {actor2}. ilgis {len(actor2)}')
+
+
+########################################################################################################################
+# 2. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus.
+# Vardą atspausdinti tik didžiosiom raidėm, o pavardę tik mažosioms. (LEONARDO dicaprio)
+# actor1 = 'Jonas Jonaitis'
+# actor2 = 'Petras Petraitis'
+# print(actor1.split()[0].upper(), actor1.split()[1].lower())
+# print(actor2.split()[0].upper(), actor2.split()[1].lower())
+
+########################################################################################################################
+# 3. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus.
+# Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš pirmų vardo ir pavardės kintamųjų raidžių.
+# Jį atspausdinti.
+# actor1 = 'Jonas Jonaitis'
+# actor2 = 'Petras Petraitis'
+# first_name1 = actor1.split()[0]
+# last_name1 = actor1.split()[1]
+# first_name2 = actor2.split()[0]
+# last_name2 = actor2.split()[1]
+# answer = first_name1[0] + last_name1[0] + first_name2[0] + last_name2[0]
+# print(answer)
+
+########################################################################################################################
+# 4. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus.
+# Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš trijų paskutinių vardo ir pavardės kintamųjų raidžių.
+# Jį atspausdinti.
+# actor1 = 'Jonas Jonaitis'
+# actor2 = 'Petras Petraitis'
+# first_name1 = actor1.split()[0]
+# last_name1 = actor1.split()[1]
+# first_name2 = actor2.split()[0]
+# last_name2 = actor2.split()[1]
+# answer1=first_name1[-3:] + last_name1[-3:]+first_name2[-3:]+last_name2[-3:]
+# print(answer1)
+
+########################################################################################################################
+# 5. Sukurti kintamąjį su stringu: "An American in Paris".
+# Jame visas “a” (didžiąsias ir mažąsias) pakeisti žvaigždutėm “*”.  Rezultatą atspausdinti.
